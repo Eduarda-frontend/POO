c78749663c2bb1f97f2710df3e9238e7f73a3a8f
Maria Eduarda <soaressilvamariaeduarda639@gmail.com>
2024-10-29 19:36:18 -0300

    correção

--- ex01.js
+++ ex01.js
@@ -1,27 +1,30 @@
-function Produto (nome, valor){
-    this.nome = nome;
-    this.valor = `R$ ${valor},00`
-}
+function Bolos (sabor, recheio){
 
-function Bolos (nome, valor, sabor, tamanho, camadas, recheio){
-    Produto.call (this, nome, valor);
     this.sabor = sabor;
-    this.tamanho = `${tamanho} cm`
-    this.camadas = camadas;
     this.recheio = recheio;
-}
-
-function Pedido(nomeCliente, telefone, nome, valor, sabor, tamanho, camadas, recheio){
-    this.nomeCliente = nomeCliente;
-    this.telefone = telefone;
-    Bolos.call(this, nome, valor, sabor, tamanho, camadas, recheio);
 
 }
 
-const bentoCake = new Pedido ('Alvin', 64999999999,'Beto Cake', 40, 'chocolate', 10, 2, 'nutella');
-const boloM = new Pedido ('Amanda', 62992655557 ,'Bolo médio',187, 'morango', 20, 3, 'ninho');
-const boloG = new Pedido ('Fernanda', 61997969893, 'Bolo Grande',225, 'baunilha', 20, 3, 'chocolate');
+function bentoCake(sabor, recheio, presente, docinhos){
 
-console.log(bentoCake);
-console.log(boloM);
-console.log(boloG);
+    Bolos.call(this, sabor, recheio);
+    this.presente = presente
+    this.docinhos = docinhos;
+
+}
+
+function boloDecorado(sabor, recheio, camadas, decoracao){
+
+    Bolos.call(this, sabor, recheio);
+    this.camadas = camadas;
+    this.decoracao = decoracao;
+
+}
+
+const cliente1 = new bentoCake ('Chocolate', 'nutella', false, 6);
+const cliente2 = new boloDecorado ('Baunilha', 'ninho com morango', 2, 'Frozen');
+const cliente3 = new bentoCake ('Baunilha', 'chocolate', true, 3);
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
